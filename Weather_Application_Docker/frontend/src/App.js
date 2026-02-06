d03af6bbb7b1d4cadce1cb4cbd8c9c29c7f415e4
Ubuntu <ubuntu@ip-172-31-12-81.ap-south-1.compute.internal>
2026-02-06 13:47:02 +0000

    updated code final

--- Weather_Application_Docker/frontend/src/App.js
+++
@@ -1,8 +0,0 @@
-import Weather from "./components/weather";
-
-
-function App() {
-  return <Weather />;
-}
-
-export default App;
--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB connected");
+  while (true) {
+    try {
+      await mongoose.connect(process.env.MONGO_URI);
+      console.log("MongoDB connected");
+      break;
+    } catch (err) {
+      console.log("Mongo not ready, retrying in 5 seconds...");
+      await new Promise((res) => setTimeout(res, 5000));
+    }
+  }
 };
 
 export default connectDB;
